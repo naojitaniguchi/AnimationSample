ebc283623fe187bcd60c74ebcc977028a99dd09b
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-06-20 11:59:35 +0900

    First commit.

---
+++ Assets/AnimChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimChange : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetTrigger("Up");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetTrigger("Down");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetTrigger("Right");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetTrigger("Left");
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Forward");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            animator.SetTrigger("Back");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AnimationSample
+[![youtube](https://img.youtube.com/vi//kzyEPEXmLQM/0.jpg)](http://www.youtube.com/watch?v=kzyEPEXmLQM "Animation transition sample.")
